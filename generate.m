ae22502b4622a896210781bb72705b7a26413626
Haoran Sun <Sunhr1993@gmail.com>
2016-10-26 14:18:06 -0500

    Add files via upload

--- generate.m
+++ generate.m
@@ -1,7 +1,7 @@
 %  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %  MATLAB code to reproduce our work on DNN research for ICASSP 2017.
 %  To run our code, Neuron Network Toolbox and Deep Learning Toolbox need to be installed first.
-%  Code has been tested successfully on MATLAB 2016b prerelease platform.
+%  Code has been tested successfully on MATLAB 2016b platform.
 %
 %  References:
 %  [1] Haoran Sun, Xiangyi Chen, Qingjiang Shi, Mingyi Hong and Xiao Fu.
@@ -12,67 +12,25 @@
 %  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function generate(K,num_H)
+tic
 Pm=1;
 var_noise = 1;
 rng('default');
-X=[];
-Y=[];
+X=zeros(K^2,num_H);
+Y=zeros(K,num_H);
 for loop = 1:num_H
     CH = 1/sqrt(2)*complex(randn(K,K), randn(K,K));
     H=abs(CH);
     temp_H = reshape(H,K^2,1);
     Pmax = Pm*ones(K,1);
     p_t = WMMSE_sum_rate(rand(K,1), H, Pmax, var_noise);
-    X=[X temp_H];
-    Y=[Y p_t];
+    X(:,loop)=temp_H;
+    Y(:,loop)=p_t;
     if mod(loop,5000)==0
         fprintf('.');
     end
 end
 save(sprintf('Gussian_%d_%d.mat',num_H, K),'X','Y');
 fprintf('Generate Done! \n');
+toc
 end
-
-
-% clear
-% clc
-% clear all
-% disp('####### PART A: Generate Training Data (speed up version) #######');
-% K = 10;
-% num_H = 50000;
-% pack=1000;
-% step=num_H/pack;
-% Pm=1;
-% rng('default');
-% var_noise = 1;
-% for FFF=1:step
-%     X=[];
-%     Y=[];
-%     for loop = 1:pack
-%         CH = 1/sqrt(2)*complex(randn(K,K), randn(K,K));
-%         H=abs(CH);
-%         temp_H = reshape(H,K^2,1);
-%         Pmax = Pm*ones(K,1);
-%         p_t = WMMSE_sum_rate(rand(K,1), H, Pmax, var_noise);
-%         X=[X temp_H];
-%         Y=[Y p_t];
-%     end
-%     save(sprintf('Gussian_%d_%d.mat',step, FFF),'X','Y');
-% end
-%
-% Xcombine=[];
-% Ycombine=[];
-% for FFF=1:step
-%     load(sprintf('Gussian_%d_%d.mat',step, FFF));
-%     Xcombine=[Xcombine X];
-%     Ycombine=[Ycombine Y];
-% end
-% X=Xcombine;
-% Y=Ycombine;
-% save(sprintf('Gussian_%d_%d.mat',num_H, K),'X','Y');
-%
-% for FFF=1:step
-%     delete(sprintf('Gussian_%d_%d.mat',step, FFF));
-% end
-% fprintf('Generate Done! \n');
-
